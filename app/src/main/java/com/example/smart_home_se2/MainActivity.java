36aed94f580bf0ff3e82cc942ec8423642feea99
steffe21dev <stefan.smudja0001@stud.hkr.se>
2019-09-11 19:14:16 +0200

    Initial Commit v1.01

--- app/src/main/java/com/example/smart_home_se2/MainActivity.java
+++ app/src/main/java/com/example/smart_home_se2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.smart_home_se2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //LIGHT ONE WIDGET
+        Switch lightOne = findViewById(R.id.switch2);
+
+
+        //DETECT STATE CHANGE OF SWITCH
+        lightOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 2019-09-11 LOGIK FÖR DB UPPSKICK
+            }
+        });
+    }
+
+    private boolean initializeSwitch(){
+
+        // TODO: 2019-09-11 Check db which state the button currently is in
+        return Boolean.parseBoolean(null);
     }
 }
