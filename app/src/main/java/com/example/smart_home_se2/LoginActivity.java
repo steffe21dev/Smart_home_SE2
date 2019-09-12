1ac9ce7a788e1e2b3bac5f08d17d3a16946f4905
Paolo Perez <pao_9517@hotmail.com>
2019-09-12 15:33:02 +0200

    Log in function(hard code username and password function).

--- app/src/main/java/com/example/smart_home_se2/LoginActivity.java
+++ app/src/main/java/com/example/smart_home_se2/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.smart_home_se2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText username = findViewById(R.id.editText);
+
+        final EditText password = findViewById(R.id.editText2);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                    System.out.println(username.getText() + "" + password.getText());
+
+
+                }else{
+                    System.out.println(username.getText() + "" + password.getText());
+
+                    Toast.makeText(LoginActivity.this, "Login fail", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
