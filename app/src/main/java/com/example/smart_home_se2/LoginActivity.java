6b74f51b3f6549bdb08c0f73df4bbbc7fabd3781
Stefan Smudja <stefan.smudja0001@stud.hkr.se>
2019-09-12 13:14:20 +0200

    Beginning of login

---
+++ app/src/main/java/com/example/smart_home_se2/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.smart_home_se2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
